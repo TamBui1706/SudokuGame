2ea486ebbd15c02d2529fb4ada5a60a7d22d1c10
tam <builehongtamk16@siu.edu.vn>
2025-02-16 18:19:10 +0700

    hi

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import SudokuApp
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = SudokuApp()
+    ex.showFullScreen()  
+    sys.exit(app.exec_())
